39397b683f68250f168e195ad2f51099578208a6
nagesh2301 <115499779+nagesh2301@users.noreply.github.com>
2022-10-30 23:11:00 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,24 @@ def newGit(repo)
 {
   git "${repo}"
 }
+
+
+def sum(a,b)
+{
+echo "sum of ${a} and ${b} is ${a+b}"
+}
+
+def sub(a,b)
+{
+echo "sub of ${a} and ${b} is ${a-b}"
+}
+
+def mul(a,b)
+{
+echo "mul of ${a} and ${b} is ${a*b}"
+}
+
+def div(a,b)
+{
+echo "div of ${a} and ${b} is ${a/b}"
+}
